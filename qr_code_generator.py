88b28517963f8ffab41e65cbbe715c5848e6f5cd
jmishic <mishicjustin@gmail.com>
2023-05-17 17:16:51 -0400

    updated for better readability

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -5,21 +5,21 @@ def mini_qr_code_maker(link: str) -> None:
     # makes the qr code using segno python library
     basic_qr_code = segno.make(link)
     # saves the qr code as a png in the project directory
-    basic_qr_code.save("title-you-want-to-save-mini-as.png")
+    basic_qr_code.save("folder-to-save-in/title-you-want-to-save-mini-as.png")
 
 
 def border_qr_code_maker(link: str) -> None:
     # makes the qr code using segno python library
     border_qr_code = segno.make(link)
     # add a border to the qr code by adding the border parameter
-    border_qr_code.save("title-you-want-to-save-border-as.png", border=5)
+    border_qr_code.save("folder-to-save-in/title-you-want-to-save-border-as.png", border=5)
 
 
 def large_qr_code_maker(link: str) -> None:
     # makes the qr code using segno python library
     large_qr_code = segno.make(link)
     # make a qr code larger by adding the scale parameter
-    large_qr_code.save("title-you-want-to-save-large-as.png",  scale=10)
+    large_qr_code.save("folder-to-save-in/title-you-want-to-save-large-as.png",  scale=10)
 
 
 def colorful_qr_code_maker(link: str) -> None:
@@ -29,16 +29,16 @@ def colorful_qr_code_maker(link: str) -> None:
     # a dark color to the dark parameter
     # change background of the qr code by setting a lighter color to the
     # light parameter (for a white/blank background, don't include this parameter)
-    large_qr_code.save("title-you-want-to-save-color-as.png", dark='darkblue', light='lightblue', scale=10)
+    large_qr_code.save("folder-to-save-in/title-you-want-to-save-color-as.png", dark='darkblue', light='lightblue', scale=10)
 
 
 def save_qr_code_as(link: str) -> None:
     # can save a qr code as .svg, .png, .eps, and .pdf
     save_as_qr_code = segno.make(link)
-    save_as_qr_code.save("title.png")
-    save_as_qr_code.save("title.svg")
-    save_as_qr_code.save("title.pdf")
-    save_as_qr_code.save("title.eps")
+    save_as_qr_code.save("folder-to-save-in/title.png")
+    save_as_qr_code.save("folder-to-save-in/title.svg")
+    save_as_qr_code.save("folder-to-save-in/title.pdf")
+    save_as_qr_code.save("folder-to-save-in/title.eps")
 
 
 def main() -> None:
